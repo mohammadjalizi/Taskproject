7a5964bee453e195f63dcc2f956add743af4ea17
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-12-23 13:07:07 +0330

    install firebase

--- src/assets/pages/linknavbar/Singnin.jsx
+++ src/assets/pages/linknavbar/Singnin.jsx
@@ -11,9 +11,19 @@ const Singnin = () => {
       
 <Navbar/>
 
-      <div className=' flex justify-center items-center bg-orange-400  '>
+      <div className='   my-44  h-[100vh] items-center  '>
 
-      Singnin
+      <h1 className='  text-center  '> singh in page </h1>
+<div className=' flex justify-center items-center my-9'>
+
+<form className='  flex flex-col gap-9   w-56 '> 
+
+<input  className=' shadow-stone-400 rounded-sm bg-gray-300  text-pink-500 '  type='email'  placeholder='Email'/>
+<input className=' shadow-stone-400 rounded-sm bg-gray-300  text-pink-500 '   type='Pasword'  placeholder='pasword'/>
+
+<button className=' bg-pink-600 rounded-2xl  shadow-2xl  '> Singnin </button>
+</form>
+</div>
 
       </div>
       <Footer/>
