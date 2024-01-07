89e9cbd641ef367538d6b1dd98e8592244fd2e6f
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2024-01-07 15:04:34 +0330

    Completed Main

--- src/assets/pages/main/Min.jsx
+++ src/assets/pages/main/Min.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { FaArrowLeft } from "react-icons/fa6";
 
 const Min = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div>
       
-<div  data-aos="fade-down-right"   className=' m-12 '>
+<div    className=' m-12  relative '>
 
-<img src="src\assets\pages\main\imag\img3.jpg" alt="" />
+<img src="src\assets\pages\main\imag\img1.jpg" alt="" />
 
+<div className=' absolute top-2/4   right-52  shadow-2xl   font-bold ' data-aos="fade-up">  
+
+<h1 className=' text-red-500'>سفارش آنلاین غذا</h1>
+<p>بهترین را دریافت کنید
+غذای با کیفیت
+از ما</p>
+<button  className=' flex items-center justify-center bg-red-500 text-white   pl-4 p-2  '  >ثبت سفارش <FaArrowLeft />
+</button>
+ </div>
 </div>
-<div data-aos="fade-up"
-     data-aos-anchor-placement="bottom-center">
-      mohammad
-</div>
+
+
+
     </div>
   )
 }
