bf7eca6b1dd34d21b7061c9250ad7f5da6895a0b
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2024-01-07 09:47:16 +0330

    Afastarry

--- src/assets/pages/main/Min.jsx
+++ src/assets/pages/main/Min.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import firstProjectPhoto from './imag/img1.jpg';
+import secondProjectPhoto from './imag/img2.jpg';
+import thirdProjectPhoto from './imag/img3.jpg';
+
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+
+const mySlider = [
+  { text: "تجربه غذای سالم و گیاهی به سبک ترخینه", link: "src/assets/pages/main/imag/img1.jpg" },
+  { text: "WOMEN", link: "src/assets/pages/main/imag/img2.jpg" },
+];
+
+
+
+
 
 const Min = () => {
+
+    
+  
   return (
-    <div>Min</div>
+    <div>
+      
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+    
+ 
+
+{mySlider.map((item)=>{
+
+return(
+
+<>
+
+<SwiperSlide className=' w-2/4 '  key={item.link}>
+<div>
+<img src={item.link} alt="" />
+</div>
+<h1 className='  absolute top-2/4 left-2/4 bg-cyan-500 ' > {item.text} </h1>
+
+</SwiperSlide>
+</>
+
+)
+
+
+})}
+     
+
+      </Swiper>
+
+
+
+    </div>
   )
 }
 
